db10a433a6fef05bf4a11f307b96243bf4d29471
Shangqing Yang <shangqingyang@Shangqings-MacBook-Air-6.local>
2025-10-13 23:13:39 +0100

    new analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import dynamic from 'next/dynamic';
 import BackgroundMusic from './components/BackgroundMusic';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // ✅ Dynamically import particle + background components to disable SSR
